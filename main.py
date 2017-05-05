1ef498ca54adfbb52e3653a3c550d3228da122b2
David Zhang <davarco123@gmail.com>
2017-05-05 11:55:03 -0700

    Initial structure

---
+++ main.py
@@ -0,0 +1,21 @@
+# df wrangling
+import numpy as np
+import pandas as pd
+import random as rnd
+
+# visualization
+import seaborn as sb
+import pylab as plt
+
+# learning libraries
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+
+
