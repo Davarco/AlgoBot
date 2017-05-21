4c0b36024558825babfc89c974d00075324c4701
VansTech <svanshaj2001@gmail.com>
2017-05-21 15:49:47 -0700

    Removed backtest methods from manager

--- src/manager.py
+++ src/manager.py
@@ -8,53 +8,6 @@ num_days = 200
 time_span = 300
 init_moneys = 2000
 
-# Prepare the pandas dataframe
-df = pd.DataFrame(columns=['Ticker', 'Spent', 'Profit', 'Percent', 'K'])
-
-
-def run_sim(stock_list: list, row: int, func_moneys: int):
-    buy = True
-    profit = 0
-    price = 0
-    total = 0
-    num = 0
-    day = 0
-    ticker = stock_list[0].ticker
-    # Go through all the stocks
-    for stock in stock_list:
-        # Get bands and current price
-        upper = stock.upper_band
-        lower = stock.lower_band
-        today = stock.today_price
-        # Buy stock if price is lower than lower band
-        if today <= lower and buy:
-            price += today
-            total += today
-            num += 1
-            buy = False
-        # Allow buying again after the lower peak
-        if today > lower and not buy:
-            buy = True
-        # Sell stock if price is higher than upper band
-        if today >= upper and num != 0:
-            profit += (today*num - price)
-            num = 0
-            price = 0
-            buy = True
-        # Change to next day
-        day += 1
-    # Calibrate moneys
-    func_moneys += profit
-    # Get the percent
-    percent = 0
-    if not profit == 0:
-        percent = profit/total*100
-    # Save data
-    df.loc[row] = ['%5s' % ticker, '%8.3f' % total, '%8.3f' % profit, '%8.3f' % percent, '%4.1f' % k]
-
-    return func_moneys
-
-
 def __main__():
     # List that holds the data
     stock_data = retrieve_list("input/companies/company_train_list.txt")
