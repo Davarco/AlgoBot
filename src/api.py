7e093c47e03c83e5ff8979eb91f04b012e2a55a8
David Zhang <davarco123@gmail.com>
2017-05-24 21:38:50 -0700

    Update api.py

--- src/api.py
+++ src/api.py
@@ -1,20 +1,4 @@
-<<<<<<< HEAD
-from flask import Flask, request
-from flask_restful import Resource, Api
-from json import dumps
-from backtest import k, num_days
-from data import retrieve_list
-from stock import Stock
 
-
-# Flask stuff
-app = Flask(__name__)
-api = Api(app)
-
-
-if __name__ == '__main__':
-    app.run()
-=======
 from flask import Flask
 from flask_restful import Resource, Api
 from data import retrieve_list
@@ -67,4 +51,4 @@ api.add_resource(SellOrder, '/stocks/sell')
 
 if __name__ == '__main__':
     app.run(debug=True)
->>>>>>> vs
+
